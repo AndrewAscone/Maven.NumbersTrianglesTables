51d62bb7fa46b4312513e384c30fffdac06befe7
AndrewAscone <andrew.ascone@gmail.com>
2022-10-31 17:30:42 -0400

    Completed all but exponent NumberUtilities

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,34 +3,69 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++){
+            if(i % 2 == 0){
+                result += i;
+            }
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++){
+            if(i % 2 != 0){
+                result += i;
+            }
+        }
+        return result;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        int result = 1;
+        String stream = "";
+        for(int i = start; i < stop; i += step){
+            result = i * i;
+            stream += result;
+        }
+        return stream;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String stream = "";
+        for (int i = 0; i < stop; i++){
+            stream += i;
+        }
+        return stream;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String stream = "";
+        for (int i = start; i < stop; i ++){
+            stream += i;
+        }
+        return stream;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String stream = "";
+        for (int i = start; i < stop; i += step){
+            stream += i;
+        }
+        return stream;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String stream = "";
+        for (int i = start; i < stop; i += step){
+            i = (int)Math.pow(i, exponent);
+            stream += i;
+        }
+        return stream;
     }
 }
